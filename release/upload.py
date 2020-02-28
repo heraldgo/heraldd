52ef785fe94e82ccc810438c8f687012cbe115d5
Xianghu Zhao <xianghuzhao@gmail.com>
2020-02-28 08:18:36 +0800

    Exit 1 if upload asset failed

--- release/upload.py
+++ release/upload.py
@@ -1,29 +1,28 @@
 #!/usr/bin/env python3
 
+# pylint: disable=missing-docstring,invalid-name
+
 import os
 import sys
 import json
-import http
 import urllib.request
 import urllib.parse
 
-http.client.HTTPConnection.debuglevel = 1
+OWNER = 'heraldgo'
+REPO = 'heraldd'
 
-owner = 'heraldgo'
-repo = 'heraldd'
+URL_RELEASES = 'https://api.github.com/repos/{owner}/{repo}/releases'.format(
+    owner=OWNER, repo=REPO)
 
-url_releases = 'https://api.github.com/repos/{owner}/{repo}/releases'.format(
-    owner=owner, repo=repo)
-
-github_token = os.environ.get('GITHUB_TOKEN')
+GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
 
 
-def get_upload_url(version):
+def get_upload_url(version):    # pylint: disable=too-many-return-statements
     release = 'v' + version
 
     try:
         resp = urllib.request.urlopen(
-            '{0}/tags/{1}'.format(url_releases, release))
+            '{0}/tags/{1}'.format(URL_RELEASES, release))
         if resp.getcode() == 200:
             return json.loads(resp.read()).get('upload_url', '')
     except urllib.error.HTTPError as e:
@@ -43,8 +42,8 @@ def get_upload_url(version):
     }
 
     data = json.dumps(new_release).encode()
-    create_request = urllib.request.Request(url_releases, data=data)
-    create_request.add_header('Authorization', 'token '+github_token)
+    create_request = urllib.request.Request(URL_RELEASES, data=data)
+    create_request.add_header('Authorization', 'token '+GITHUB_TOKEN)
 
     try:
         resp = urllib.request.urlopen(create_request)
@@ -71,22 +70,23 @@ def upload_asset(url_upload, file_path):
 
     upload_request = urllib.request.Request(
         '{0}?name={1}'.format(url_upload, file_name), data=data)
-    upload_request.add_header('Authorization', 'token '+github_token)
+    upload_request.add_header('Authorization', 'token '+GITHUB_TOKEN)
     upload_request.add_header('Content-Type', 'application/zip')
 
     try:
         resp = urllib.request.urlopen(upload_request)
         if resp.getcode() != 201:
             print('Failed to upload asset', file=sys.stderr)
-            return
+            return False
     except urllib.error.HTTPError as e:
         print('Upload asset HTTP error', e.code, e.reason, file=sys.stderr)
-        return
+        return False
     except OSError as e:
         print('Upload asset error:', e, file=sys.stderr)
-        return
+        return False
 
     print('Upload file successfully:', file_path)
+    return True
 
 
 def main():
@@ -97,7 +97,7 @@ def main():
     version = sys.argv[1]
     assets = sys.argv[2:]
 
-    if not github_token:
+    if not GITHUB_TOKEN:
         print('Github token not provided', file=sys.stderr)
         return 1
 
@@ -109,10 +109,12 @@ def main():
     url_upload = upload_url.split('{', 1)[0]
     print("URL upload:", url_upload)
 
+    all_upload_success = True
     for f in assets:
-        upload_asset(url_upload, f)
+        if not upload_asset(url_upload, f):
+            all_upload_success = False
 
-    return 0
+    return 0 if all_upload_success else 1
 
 
 if __name__ == '__main__':
